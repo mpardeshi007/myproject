6086d58d433b4396feae40055eebf3e2dc3c49c5
mahesh <mpardeshi007@gmail.com>
2017-09-27 00:22:43 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
-
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+//import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl : './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';
+  results: string[];
+  constructor(private http: HttpClient,public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.items = af.list('/messages', {
+      query: {
+        limitToLast: 50
+      }
+    });
+    
+    this.user = this.afAuth.authState;
+  }
+  ngOnInit(): void {
+    // Make the HTTP request:
+    this.http.get('http://www.myapifilms.com/imdb/idIMDB?name=Shah+Rukh+Khan&token=5b4a5a87-273b-456a-9c88-711b44ef0896&format=json&language=en-us&filmography=1&exactFilter=0&limit=10&bornDied=0&starSign=0&uniqueName=0&actorActress=0&actorTrivia=0&actorPhotos=0&actorVideos=0&salary=0&spouses=0&tradeMark=0&personalQuotes=0&starMeter=0&fullSize=0&alternateNames=0').subscribe(data => {
+      console.log(data);
+    });
+  }
+  login() {
+    this.afAuth.auth.signInAnonymously();
+   }
+
+    logout() {
+        this.afAuth.auth.signOut();
+    }
+
+    Send(desc: string) {
+        this.items.push({ message: desc});
+        this.msgVal = '';
+    }
 }
+
+// const imdb = require('imdb-api');
+
+// imdb.get('The Toxic Avenger', {apiKey: 'foo', timeout: 30}).then(console.log).catch(console.log);
+// imdb.getById('tt0090190', {apiKey: 'foo', timeout: 30}).then(console.log).catch(console.log);
+// imdb.getReq({ name: 'The Toxic Avenger', opts: {apiKey: 'foo', timeout: 30} }).then(console.log).catch(console.log);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
+import { AngularFireModule } from 'angularfire2';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { AppComponent }  from './app.component';
+import { HttpModule }    from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+   apiKey: "AIzaSyCt3MlKksspitVIbUsMs5T9_FyTuG0PHJA",
+    authDomain: "myapp-07.firebaseapp.com",
+    databaseURL: "https://myapp-07.firebaseio.com",
+    projectId: "myapp-07",
+    storageBucket: "myapp-07.appspot.com",
+    messagingSenderId: "892492656854"
+};
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule ,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    HttpClientModule
+// <-- import the FormsModule before binding with [(ngModel)]
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ],
+  providers :[]
+  
 })
 export class AppModule { }
+
